2379c04063abe93cdc763fe0ee91157d18121723
mdalmamunit427 <mdalmamunit427@gmail.com>
2025-12-10 13:54:15 +0600

    typing-effect-python

---
+++ typing-effect-message-python/text_gf.py
@@ -0,0 +1,8 @@
+confession = input("I like you, do you like me? (yes/no): ")
+if confession.lower() == "yes":
+    print("Yayyy! 💖")
+
+elif confession.lower() == "no":
+    print("It's okay, I still like you! 💖😁")
+else:
+    print("Invalid input, but I'll take that as a maybe! 😉")
